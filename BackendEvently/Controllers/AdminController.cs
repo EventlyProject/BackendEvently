e3d35fe346fc63451a521f9474e343b152606dbb
DAH <DAH@PROELE18>
2025-05-23 12:31:01 +0200

    Added Services And Interfaces

--- BackendEvently/Controllers/AdminController.cs
+++ BackendEvently/Controllers/AdminController.cs
@@ -1,6 +1,24 @@
-﻿namespace BackendEvently.Controllers
+﻿using BackendEvently.Data;
+using BackendEvently.Dtos;
+using BackendEvently.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendEvently.Controllers
 {
-    public class AdminController
+    [Route("api/admin")]
+    [ApiController]
+    [Authorize(Roles = "Admin")]
+    public class AdminController : ControllerBase
     {
+        private readonly ApplicationDBContext _context;
+        private readonly IEmailService _emailService;
+
+        public AdminController(ApplicationDBContext context, IEmailService emailService)
+        {
+            _context = context;
+            _emailService = emailService;
+        }
     }
 }
